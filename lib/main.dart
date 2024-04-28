6757cf511e81fedcd0b23ecf1e6400c614613eed
songbirdzz <165868972+songbirdzz@users.noreply.github.com>
2024-04-27 18:45:15 -0700

    Fix: Unable to initialize on mobile devices

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:hive_flutter/adapters.dart';
 import 'package:knkpanime/app_module.dart';
 import 'package:knkpanime/app_widget.dart';
 import 'package:knkpanime/utils/storage.dart';
+import 'package:knkpanime/utils/utils.dart';
 import 'package:media_kit/media_kit.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:window_manager/window_manager.dart';
@@ -14,6 +15,8 @@ void main() async {
   await Hive.initFlutter('${(await getApplicationSupportDirectory()).path}/v1');
   await Storage.init();
   MediaKit.ensureInitialized();
-  await windowManager.ensureInitialized();
+  if (Utils.isDesktop()) {
+    await windowManager.ensureInitialized();
+  }
   runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
